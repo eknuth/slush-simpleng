63725ddd2fb951087403f756ee3e3975bda2b566
Edwin Knuth <eknuth@gmail.com>
2015-04-08 20:11:53 -0700

    Beginning to break out apps task.

--- slushfile.js
+++ slushfile.js
@@ -8,23 +8,30 @@
 
 'use strict';
 
-var gulp = require('gulp');
-var path = require('path');
+var gulp        = require('gulp');
+var path        = require('path');
+var install     = require('gulp-install');
+var conflict    = require('gulp-conflict');
+var template    = require('gulp-template');
+var rename      = require('gulp-rename');
+var _            = require('underscore.string');
+var inquirer    = require('inquirer');
+var fs          = require('fs');
 
 function format(string) {
     var username = string.toLowerCase();
     return username.replace(/\s/g, '');
 }
 
-var defaults = (function () {
+
+var defaults = (function() {
     var workingDirName = path.basename(process.cwd()),
-      homeDir, osUserName, configFile, user;
+        homeDir, osUserName, configFile, user;
 
     if (process.platform === 'win32') {
         homeDir = process.env.USERPROFILE;
         osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
-    }
-    else {
+    } else {
         homeDir = process.env.HOME || process.env.HOMEPATH;
         osUserName = homeDir && homeDir.split('/').pop() || 'root';
     }
@@ -32,10 +39,9 @@ var defaults = (function () {
     configFile = path.join(homeDir, '.gitconfig');
     user = {};
 
-    if (require('fs').existsSync(configFile)) {
+    if (fs.existsSync(configFile)) {
         user = require('iniparser').parseSync(configFile).user;
     }
-
     return {
         appName: workingDirName,
         userName: osUserName || format(user.name || ''),
@@ -46,5 +52,57 @@ var defaults = (function () {
 })();
 
 gulp.task('default', function (done) {
-    require('./tasks/app').task(defaults, done);
+    var prompts = [{
+        name: 'appName',
+        message: 'What is the name of your project?',
+        default: defaults.appName
+    }, {
+        name: 'componentName',
+        message: 'What is the name of your angular component?',
+        default: defaults.componentName
+    }, {
+        name: 'appDescription',
+        message: 'What is the description?'
+    }, {
+        name: 'appVersion',
+        message: 'What is the version of your project?',
+        default: '0.1.0'
+    }, {
+        name: 'authorName',
+        message: 'What is the author name?',
+        default: defaults.authorName
+    }, {
+        name: 'authorEmail',
+        message: 'What is the author email?',
+        default: defaults.authorEmail
+    }, {
+        name: 'userName',
+        message: 'What is the github username?',
+        default: defaults.userName
+    }, {
+        type: 'confirm',
+        name: 'moveon',
+        message: 'Continue?'
+    }];
+    //Ask
+    inquirer.prompt(prompts,
+        function(answers) {
+            if (!answers.moveon) {
+                return done();
+            }
+            answers.appNameSlug = _.slugify(answers.appName);
+            gulp.src(__dirname + '/templates/app/**')
+                .pipe(template(answers))
+                .pipe(rename(function(file) {
+                    if (file.basename[0] === '_') {
+                        file.basename = '.' + file.basename.slice(1);
+                    }
+                }))
+                .pipe(conflict('./'))
+                .pipe(gulp.dest('./'))
+                .pipe(install())
+                .on('end', function() {
+                    done();
+                });
+        });
 });
